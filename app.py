296ec0037e84167d48484b755c905f0d7fbe0626
uuupah <uuupah@gmail.com>
2021-01-13 22:31:13 +1030

    implement start screen and node input

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import curses
 
 def main(stdscr):
-    w = 40
+    w = 80
     h = 24
-    states = [[0 for x in range(w)] for y in range(h)]
+    states = [[1 for x in range(w)] for y in range(h)]
 
+    screen_state = 'welcome'
+
+    curses.start_color()
+    # curses.use_default_colors()
     curses.curs_set(0)
     curses.mousemask(1)
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     stdscr.addstr(1, 1, "welcome to conway's game of life")
     stdscr.addstr(3, 1, "click any location to toggle a cell's living state")
@@ -20,9 +24,27 @@ def main(stdscr):
         if key == curses.KEY_MOUSE:
             _, x, y, _, _ = curses.getmouse()
 
-            stdscr.attron(curses.color_pair(1))
-            stdscr.addch(y, x, " ")
-            stdscr.attroff(curses.color_pair(1))
+            # delete content 
+            if (screen_state == 'welcome'):
+                stdscr.clear()
+                curses.endwin()
+                screen_state = 'setup'
+
+            # stdscr.addstr(0, 0, "({},{}))".format(y, x))
+
+            if states[y][x] == 1:
+                stdscr.attron(curses.color_pair(2))
+                stdscr.addch(y, x, " ")
+                stdscr.attroff(curses.color_pair(2))
+
+                states[y][x] = 2
+
+            elif states[y][x] == 2:
+                stdscr.attron(curses.color_pair(1))
+                stdscr.addch(y, x, " ")
+                stdscr.attroff(curses.color_pair(1))
+
+                states[y][x] = 1
 
         elif key == 27:
             break
